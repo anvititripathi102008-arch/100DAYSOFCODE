885844114e37e2ec18bffd86f27203e523385e6a
anvititripathi102008-arch <anvititripathi102008@gmail.com>
2026-08-24 11:28:26 +0530

    Add files via upload

---
+++ digits.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a=123;
+int b=12;
+printf("the two numbers are a=123 and b=12 \n");
+int sum =a+b;
+int diff =a-b;
+int pro =a*b;
+int div =a/b;
+int rem =a%b;
+printf("\n the sum of a and b is %d", sum);
+printf("\n the diff of a and b is  %d", diff);
+printf("\n the pro of a and b is  %d", pro);
+printf("\n the div of a and b is %d", div);
+printf("\n the rem of a and b is  %d", rem);
+return 0;
+}
+
